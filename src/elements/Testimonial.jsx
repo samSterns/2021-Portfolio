0e24267060e571790b01bd91801af72251c12a5e
samSterns <sam.g.sterns@gmail.com>
2021-06-15 14:37:52 -0700

    links added to testimonial

--- src/elements/Testimonial.jsx
+++ src/elements/Testimonial.jsx
@@ -18,7 +18,7 @@ class Testimonial extends Component{
                                         <p>"Sam is a tenacious human who takes on their work with an open mind! Their creative thinking is a real asset to any organization. As Sam's mentor, I have seen and heard first hand about how they approach projects, their adaptability and most importantly, their humility in willing to learn about an organization's true north and approach to coding and accessible design."</p>
                                     </div>
                                     <div className="author-info">
-                                        <h6><span>Hazel Valdez </span> - Associate Director, Oregon Bioscience Association and <em>PDX Women In Tech Ambassador</em>
+                                        <h6><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/hazel-valdez/" >Hazel Valdez </a> - Associate Director, Oregon Bioscience Association and <em>PDX Women In Tech Ambassador</em>
 </h6>
 
                                     </div>
@@ -31,7 +31,7 @@ class Testimonial extends Component{
                                         <p>Fatima Asrafy standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
                                     </div>
                                     <div className="author-info">
-                                        <h6><span>Serge Lubensky </span> - Founder at ReTool Ventures | Tech Entrepreneur</h6>
+                                        <h6><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/slubensky/">Serge Lubensky </a> - Founder at ReTool Ventures | Tech Entrepreneur</h6>
                                     </div>
                                 </div>
                             </TabPanel>
@@ -41,7 +41,7 @@ class Testimonial extends Component{
                                         <p>Jannat Tumpa The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections Bonorum et Malorum original.</p>
                                     </div>
                                     <div className="author-info">
-                                        <h6><span>Jannat Tumpa </span> - COO, AMERIMAR ENTERPRISES, INC.</h6>
+                                        <h6><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kitkohler/"> Kit Kohler </a> - COO, AMERIMAR ENTERPRISES, INC.</h6>
                                     </div>
                                 </div>
                             </TabPanel>
@@ -110,14 +110,14 @@ class Testimonial extends Component{
                                 <Tab>
                                     <div className="testimonial-thumbnai">
                                         <div className="thumb">
-                                            <img src="/assets/images/client/testimonial-2.jpg" alt="Testimonial Images"/>
+                                            <img src="/assets/images/client/testimonial-3.jpg" alt="Testimonial Images"/>
                                         </div>
                                     </div>
                                 </Tab>
                                 <Tab>
                                     <div className="testimonial-thumbnai">
                                         <div className="thumb">
-                                            <img src="/assets/images/client/testimonial-3.jpg" alt="Testimonial Images"/>
+                                            <img src="/assets/images/client/testimonial-2.jpg" alt="Testimonial Images"/>
                                         </div>
                                     </div>
                                 </Tab>
