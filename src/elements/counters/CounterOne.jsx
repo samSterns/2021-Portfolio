6e055036b3ade86093a82f1427e66ceaf3a68479
samSterns <sam.g.sterns@gmail.com>
2021-06-24 09:45:04 -0700

    fixed counter and grammar

--- src/elements/counters/CounterOne.jsx
+++ src/elements/counters/CounterOne.jsx
@@ -32,12 +32,12 @@ class CounterOne extends Component{
                 <div className="row">
                     {Data.map( (value, index) => (
                         <div className="counterup_style--1 col-lg-4 col-md-4 col-sm-6 col-12" key={index}>
+                            <p className="description">{value.countTitle}</p>
                             <h5 className="counter">
                                 <VisibilitySensor onChange={this.onVisibilityChange} offset={{top:10}} delayedCall>
                                     <CountUp end={this.state.didViewCountUp ? value.countNum : 0} />
                                 </VisibilitySensor>
                             </h5>
-                            <p className="description">{value.countTitle}</p>
                         </div>
                     ))}
                 </div>
